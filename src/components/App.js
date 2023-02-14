3de0031ec3490abff21630f4d16be0f0321e3c91
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-02-14 12:42:20 +0530

    Solution updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  let handleClick = (e) => {setIsVisible(true)}
+
+  useEffect(()=>{}, [isVisible])
+
+  let [isVisible, setIsVisible] = useState(false)
   return (
     <div id="main">
       // Do not alter the main div
+     {isVisible && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button id="click" onClick={handleClick}>Click me</button>
     </div>
   );
 }
